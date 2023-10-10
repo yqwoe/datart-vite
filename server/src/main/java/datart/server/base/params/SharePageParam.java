df75924d677df5cd634b4862a5f2d81b355d849d
yqwoe <yqwoe@live.cn>
2023-10-10 10:17:43 +0800

    feat: cra切换至vite；地图下钻；等一些新的特性

--- server/src/main/java/datart/server/base/params/SharePageParam.java
+++
@@ -1,9 +0,0 @@
-package datart.server.base.params;
-
-import lombok.Data;
-
-@Data
-public class SharePageParam {
-    String url;
-    String password;
-}
--- server/src/main/resources/db/migration/V2023.07.03__1.0.1.rc.1.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `user`
-    ADD COLUMN `phone` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
